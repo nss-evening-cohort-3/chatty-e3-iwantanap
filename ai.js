db9490829de5b082b144e2b514b33f8c706d11da
Paul <wip321@yahoo.com>
2016-03-22 01:13:24 -0500

    AI Added AI JSON and AI JavaScript. Made it able to pick a random respose. Also it can find certain words in the previous message. Making it more user friendly. I think we should make it so you can't edit or delete these AI posts. But I'll save that for later.

---
+++ ai.js
@@ -0,0 +1,30 @@
+var Chatty = (function(oldChatty) {
+  var ai = []
+  var aiXML = new XMLHttpRequest();
+  aiXML.addEventListener("load", function(){
+    ai = JSON.parse(this.responseText)
+    console.log(ai)
+  });
+  aiXML.open("GET", "ai.json");
+  aiXML.send();
+
+  oldChatty.chooseAI = function() {
+    let textAI = {}
+    var msgString = Chatty.getMessages()[Chatty.getMessages().length-1].message
+    if (msgString.indexOf("?") >= 0) {
+      textAI = ai.answers[Math.floor(Math.random()* ai.answers.length)]
+    } else if (msgString.toLowerCase().indexOf("hello") >=0) {
+      textAI = ai.hello[Math.floor(Math.random()* ai.hello.length)]
+    } else if (msgString.toLowerCase().indexOf("bye") >=0) {
+      textAI = ai.bye[Math.floor(Math.random()* ai.bye.length)]
+    } else if (msgString.toLowerCase().indexOf("die") >=0) {
+      textAI = ai.die[Math.floor(Math.random()* ai.die.length)]
+    } else {
+      textAI = ai.random[Math.floor(Math.random()* ai.random.length)]
+    }
+    Chatty.addMessages(textAI)
+    Chatty.onToDom()
+  }
+
+  return Chatty
+}(Chatty));
--- eventListeners.js
+++ eventListeners.js
@@ -7,6 +7,7 @@ chattyBody.addEventListener("keypress", function(e) {
   // NAV: When return key is detected, you'll create a new message.
   if (event.target.id === "message-input" && event.code === "Enter") {
     Chatty.createNewMessage();
+    setTimeout(Chatty.chooseAI, Math.floor(Math.random()* 5000))
   }
 });
 
