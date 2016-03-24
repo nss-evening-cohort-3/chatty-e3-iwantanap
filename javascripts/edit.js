799542f3f1bfc929b301a3911e2d5b42e4b338bb
Callan Morrison <callan.l.morrison@gmail.com>
2016-03-24 00:54:36 -0500

    completed commenting for edit

--- javascripts/edit.js
+++ javascripts/edit.js
@@ -2,34 +2,35 @@
 
 var Chatty = ((oldChatty) => {
 
+    //Edit mode func (ChattyAugmentation) first disables edit mode button for all messages to prevent bug where first-selected message text is applied to all messages in editMode 
+    //Gathers id, message content
     oldChatty.editModeFunc = (thisMessage) => {
-        let messageId = thisMessage.id;
         Chatty.disableEditMode();
-        let messageToEditText = thisMessage.getElementsByTagName("p")[1];
-        let messageValue = messageToEditText.innerHTML
-        messageValue = messageValue.replace(/<p.*<\/p>/g, "");
-        messageValue = Chatty.removeEmoji(messageValue);
-        messageToEditText.innerHTML = `<input type="text" id="editMode">`;
-        let editMode = document.getElementById("editMode")
-        editMode.value = messageValue;
+        let messageId = thisMessage.id;  //gathers message ID
+        let messageToEditText = thisMessage.getElementsByTagName("p")[1]; //gathers message content
+        let messageValue = messageToEditText.innerHTML;  //gathers message content
+        messageValue = messageValue.replace(/<p.*<\/p>/g, "");  //removes "edited tag" from any messages previously edited when user puts a message into edit mode multiple times
+        messageValue = Chatty.removeEmoji(messageValue); //puts any images into regular [] form for edit purposes
+        messageToEditText.innerHTML = `<input type="text" id="editMode">`; //inserts text input mode where message text was displayed
+        let editMode = document.getElementById("editMode"); //selects element in editMode
+        editMode.value = messageValue; //inserts current message text as default value for text input
         editMode.addEventListener("keypress", (e) => {
             if (event.code === "Enter") {
-                let edittedText = editMode.value;
-                edittedText = Chatty.addEmoji(edittedText);
-                let editTimestamp = new Date();
-                let edittedAt = `<p class="editedTag"> edited at ${editTimestamp}</p>`;
-                messageToEditText.innerHTML = edittedText 
-                Chatty.editMessage(messageId, edittedText, edittedAt);
-                Chatty.onToDom()
+                let edittedText = editMode.value; //grabs input value and stores it in a new var
+                edittedText = Chatty.addEmoji(edittedText); //transforms and [] elements back into emojis
+                let editTimestamp = new Date(); //creates timestamp for edited time
+                let edittedAt = `<p class="editedTag"> edited at ${editTimestamp}</p>`; //adds element that lets users know the last time the message was edited
+                messageToEditText.innerHTML = edittedText //replaces message content area with new updated message
+                Chatty.editMessage(messageId, edittedText, edittedAt); //updates private message array
+                Chatty.onToDom() //updates DOM with new array, also reactivates all edit buttons
             }
         });
     }
 
     oldChatty.disableEditMode = () => {
-        
-        let editButton = document.getElementsByClassName("editThisMessage");
+        let editButton = document.getElementsByClassName("editThisMessage"); //grabs all edit buttons for messages currently displayed on dom
         for (let i = 0; i < editButton.length; i++) {
-            editButton[i].disabled = true;
+            editButton[i].disabled = true; //loops through each edit btton and disables it temporarily - it is editable once user leaves edit mode and items are added to the DOM
         };
     }
 
