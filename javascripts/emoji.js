cc0b8f54f6a27b2007d36e8c4dde1a5941120d87
Bernard Anderson <bernard.anderson@gmail.com>
2016-03-23 21:03:08 -0500

    ES6 Fixes and comments for three js's Updated the js to ES6 and comments for chatty.js, createmMessages.js and emoji.js files

--- javascripts/emoji.js
+++ javascripts/emoji.js
@@ -2,32 +2,33 @@
 
 var Chatty = ( (oldChatty) => {
 
-// The current emoji list
+  // The current emoji list of [] === <img>
   let emojiList = {
-      "[happy]": '<img src="emoji/happy.png">',
-      "[sleepy]": '<img src="emoji/sleepy.png">',
-      "[poopy]": '<img src="emoji/poopy.png">'
+    "[happy]": '<img src="emoji/happy.png">',
+    "[sleepy]": '<img src="emoji/sleepy.png">',
+    "[poopy]": '<img src="emoji/poopy.png">'
   };
 
-// Adds Emoji to any newly commited message
+  // Converts Emoji [ ] tags to <img> tags for any message text
   oldChatty.addEmoji = (sentMessageString) => {
     for (let emojiKey in emojiList) {
       if (sentMessageString.includes(emojiKey)) {
-          sentMessageString = sentMessageString.replace(emojiKey, emojiList[emojiKey]);
+        sentMessageString = sentMessageString.replace(emojiKey, emojiList[emojiKey]);
       };
     };
-      return sentMessageString;
-  };
+    return sentMessageString;
+  }
 
-  // Adds Emoji to any newly commited message
+  // Removes Emoji <img> tags for messages so the returned message text
+  //  looks like what the user type (for editing)
   oldChatty.removeEmoji = (sentMessageString) => {
     for (let emojiKey in emojiList) {
       if (sentMessageString.includes(emojiList[emojiKey])) {
-          sentMessageString = sentMessageString.replace(emojiList[emojiKey], emojiKey);
+        sentMessageString = sentMessageString.replace(emojiList[emojiKey], emojiKey);
       };
     };
-      return sentMessageString;
-  };
+    return sentMessageString;
+  }
 
   return oldChatty
 
