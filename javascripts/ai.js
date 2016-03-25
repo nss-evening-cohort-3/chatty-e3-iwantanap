f13220a48b811da535623b9eede120f1698cd5be
Paul <wip321@yahoo.com>
2016-03-24 20:30:00 -0500

    Added comments to these three .js files

--- javascripts/ai.js
+++ javascripts/ai.js
@@ -1,22 +1,31 @@
 "use strict"
 var Chatty = (function(oldChatty) {
+  //Array for the responses to be stored into
   let ai = [];
+  //Starts XHR
   let aiXML = new XMLHttpRequest();
   aiXML.addEventListener("load", function(){
     ai = JSON.parse(this.responseText);
   });
   aiXML.open("GET", "ai.json");
   aiXML.send();
+  //Ends XHR
 
+  //Chooses which 'AI' response to post
   oldChatty.chooseAI = function() {
     let textAI = {};
     let msgString = Chatty.getMessages()[Chatty.getMessages().length-1].message;
+    //Checks for different reponse variables
     if (msgString === "") {
       textAI = ai.empty[Math.floor(Math.random()* ai.empty.length)];
     } else if (msgString.indexOf("?") >= 0) {
       textAI = ai.answers[Math.floor(Math.random()* ai.answers.length)];
     } else if (msgString.toLowerCase().indexOf("hello") >=0) {
       textAI = ai.hello[Math.floor(Math.random()* ai.hello.length)];
+    } else if (msgString.toLowerCase().indexOf("cat") >=0) {
+      textAI = ai.cat[Math.floor(Math.random()* ai.cat.length)];
+    } else if (msgString.toLowerCase().indexOf("dog") >=0) {
+      textAI = ai.dog[Math.floor(Math.random()* ai.dog.length)];
     } else if (msgString.toLowerCase().indexOf("bye") >=0) {
       textAI = ai.bye[Math.floor(Math.random()* ai.bye.length)];
     } else if (msgString.toLowerCase().indexOf("die") >=0) {
@@ -26,11 +35,13 @@ var Chatty = (function(oldChatty) {
     } else {
       textAI = ai.random[Math.floor(Math.random()* ai.random.length)];
     }
+    //Gives timestamp to responses
     textAI.timestamp = new Date();
     Chatty.addMessages(textAI);
     Chatty.onToDom();
   }
 
+  //Prevents user from editing or deleting AI responses
   oldChatty.stopEditDelete = function() {
     let runThrough = document.getElementsByClassName('container-fluid');
     for (let i in runThrough) {
