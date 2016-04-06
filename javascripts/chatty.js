8fa17ae12f0aaa1a180bbf60c20c785560e60e16
Callan Morrison <callan.l.morrison@gmail.com>
2016-04-06 18:06:14 -0500

    added jquery to chatty

--- javascripts/chatty.js
+++ javascripts/chatty.js
@@ -5,31 +5,12 @@ var Chatty = (() => {
   let messages = [];
 
   return {
-    
     loadMessages: (callbackFunc) => {
       // The first IIFE should add a public function (e.g. loadMessages) that loads the messages.json file and stores the messages in a private variable.
-      //Step 1: Set up http req for messages
-      let messagesReq = new XMLHttpRequest;
-
-      //Step 2: Go get it
-      messagesReq.open("GET", "messages.json");
-      messagesReq.send();
-
-      //Step 3: Event Listener
-      messagesReq.addEventListener("load", messagesSuccess);
-      messagesReq.addEventListener("failed", failedExecution);
-
-       //Step 4: If json fails to load
-      function failedExecution() {
-        alert("Error loading page. Please refresh.")
-      };
-
-      //Step 5: Translate into JS
-      //Step 6: Create callback for once the messages page loads
-      function messagesSuccess() {
-        messages = JSON.parse(this.responseText).messages;
+      $.get("messages.json", function(data){
+        messages = data.messages;
         callbackFunc();
-      };
+      });
     },
 
     //Adds new message text to the messages Array
@@ -44,14 +25,14 @@ var Chatty = (() => {
     //Function takes the message edit target Array element and adds the edited message text
     // and edit-timestamp to that array element
     editMessage: (idOfText, messageText, editedId) => {
-      for (let i = 0; i < messages.length; i++) {
+      $.each(messages, function(i) {
         if (messages[i].id == idOfText) {
           messages[i].message = messageText;
           messages[i].editted = editedId;
-          break;  
-        };
-      };
-    },
+          }
+        });
+      },
+    
     replaceMessages: (newArray) => {messages = newArray}
   };
 })();
